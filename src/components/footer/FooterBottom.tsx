b6f7bf7fb247d599a5568ded47e4f74e581b7ce2
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-09 00:12:14 +0100

    FooterBotton.tsx - footerDate created

--- src/components/footer/FooterBottom.tsx
+++ src/components/footer/FooterBottom.tsx
@@ -2,7 +2,9 @@ import '../../assets/css/footer/footerBotton.css';
 
 const FooterBotton = () => {
     return(
-        <div id="footerBottom"></div>
+        <div id="footerBottom">
+            <div id="footerDate"><span style={{color: 'white'}}>Coming 4</span><span style={{color: '#5175FF'}}> Nov 2020</span></div>
+        </div>
     )
 }
 
