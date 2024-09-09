a726fb502ef8dc819c137c908cd3067163c93624
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-09 14:19:04 +0100

    FooterBotton.tsx - code cleaning

--- src/components/home/footer/FooterBottom.tsx
+++ src/components/home/footer/FooterBottom.tsx
@@ -1,14 +1,11 @@
 import '../../../assets/css/home/footer/footerBotton.css';
-import '../../../assets/css/home/footer/homeFooter.css';
 import DefaultButton from '../../commons/DefaultButton';
 import BackCounter from '../../commons/BackCounter';
 const FooterBotton = () => {
     return (
         <div id="footerBottom">
-            <div id="footerCounter">
-                <BackCounter/>
-                <DefaultButton buttonType='getStarted'>Get Started</DefaultButton>
-            </div>
+            <BackCounter />
+            <DefaultButton buttonType='getStarted'>Get Started</DefaultButton>
         </div>
     )
 }
