d680fdaee5060e8ea93e8f0d69946c507433eac3
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-10 22:39:48 +0100

    Option.tsx - checked stated done

--- src/components/commons/formElements/select/option/Option.tsx
+++ src/components/commons/formElements/select/option/Option.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
 import './option.css';
 type Props = {
     id:string;
+    checked:boolean
     pack:string;
     price:string;
 }
 
-const Option: React.FC<Props> = ({id,pack,price})=>{
+const Option: React.FC<Props> = ({id,checked,pack,price})=>{
+    
+    const [optionChecked, setOptionChecked] = useState("option");
+
+    useEffect(() => {
+        checked ? setOptionChecked("option checked") : setOptionChecked("option");
+    }, [checked]);
+
     return(
-        <div id={id} className="option"><span className="pack">{pack} </span><span className="price">{price}</span></div>
+        <div id={id} className={optionChecked}><span className="pack">{pack} </span><span className="price">{price}</span></div>
     )
 }
 export default Option;
--- src/components/signUp/SignUpForm/SignUpForm.tsx
+++ src/components/signUp/SignUpForm/SignUpForm.tsx
@@ -11,9 +11,9 @@ const SignUpForm = () => {
                 <TextField id='name'>Name</TextField>
                 <TextField id='email'>Email Addres</TextField>
                 <Select id='plan'>
-                    <Option id="free" pack="Basic Pack" price="Free" />
-                    <Option id="pro" pack="Pro Pack" price="$9.99" />
-                    <Option id="ultimate" pack="Ultimate Pack" price="$19.99" />
+                    <Option id="free" checked={true} pack="Basic Pack" price="Free" />
+                    <Option id="pro" checked={false} pack="Pro Pack" price="$9.99" />
+                    <Option id="ultimate" checked={false} pack="Ultimate Pack" price="$19.99" />
                 </Select>
                 <TextField id='phone'>Phone Number</TextField>
                 <TextField id='company'>Company</TextField>
