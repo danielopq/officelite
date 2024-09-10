f2ae79ff3d2e8b55ce97bd1a477778bbfbf723b8
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-10 20:26:44 +0100

    Select.tsx - selectSubmenu general layout

--- src/components/commons/formElements/select/Select.tsx
+++ src/components/commons/formElements/select/Select.tsx
@@ -11,8 +11,8 @@ const Select: React.FC<Props> = ({ id, children }) => {
     return (
         <div className='formContainer'>
             <div id={id} className='select'>
-                <div id="selectHeader" className='arrowDown'>
-                    <p id="selectedValue"><span className="pack">Basic Pack </span><span className="price">Free</span></p>
+                <div id="selectHeader">
+                    <p id="selectedValue"  className='arrowDown'><span className="pack">Basic Pack </span><span className="price">Free</span></p>
                 </div>
                 <div id="selectSubmenu">
                     {children}
