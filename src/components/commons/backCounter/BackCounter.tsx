f416dde8aa7e2c35db27bf68251bb02dfa368baa
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-10 15:08:19 +0100

    BackCounter.tsx - adapted to dark and light version

--- src/components/commons/backCounter/BackCounter.tsx
+++ src/components/commons/backCounter/BackCounter.tsx
@@ -1,28 +1,61 @@
 import './backCounter.css';
+import { useState } from 'react';
 
-const BackCounter = () => {
+type props = {
+    bcType: string;
+}
+
+interface classNamestate {
+    lapse: string;
+    comingLabel: string;
+    lapseNumber: string;
+    lapseType: string;
+}
+
+const BackCounter: React.FC<props> = ({ bcType }) => {
+
+    const [classNameState, setClassName] = useState<classNamestate>({
+        lapse: 'lapse lapseDark',
+        comingLabel: 'comingLabelDark',
+        lapseNumber: 'lapseNumber lapseNumberDark',
+        lapseType: 'lapseType lapseTypeDark',
+    });
+
+    (() => {
+        if(bcType !== 'dark'){
+            setClassName({
+                lapse: 'lapse lapseLight',
+                comingLabel: 'comingLabelLight',
+                lapseNumber: 'lapseNumber lapseNumberLight',
+                lapseType: 'lapseType lapseTypeLight',
+            })
+        }
+    })()
+
+    const { lapse, comingLabel, lapseNumber, lapseType } = classNameState;
+
     return (
         <div id="backCounter">
             <div id="expiringDate">
-                <span id="final-coming" style={{ color: 'black' }}>COMING 4</span>
-                <span id="final-date" style={{ color: '#5175FF' }}> NOV 2020</span>
+                <span className={comingLabel}>COMING 4</span>
+                <span id="dateLabel"> NOV 2020</span>
             </div>
             <div id="counter">
-                <div className='lapse'>
-                    <div className='lapseNumber'>47</div>
-                    <div className='lapseType'>days</div>
+                <div className={lapse}>
+                    <div className={lapseNumber}>47</div>
+                    <div className={lapseType}>days</div>
                 </div>
-                <div className='lapse'>
-                    <div className='lapseNumber'>07</div>
-                    <div className='lapseType'>hours</div>
+                <div className={lapse}>
+                    <div className={lapseNumber}>07</div>
+                    <div className={lapseType}>hours</div>
                 </div>
-                <div className='lapse'>
-                    <div className='lapseNumber'>56</div>
-                    <div className='lapseType'>min</div>
+                <div className={lapse}>
+                    <div className={lapseNumber}>56</div>
+                    <div className={lapseType}>min</div>
                 </div>
-                <div className='lapse'>
-                    <div className='lapseNumber'>14</div>
-                    <div className='lapseType'>sec</div>
+                <div className={lapse}>
+                    <div className={lapseNumber}>14</div>
+                    <div className={lapseType}>sec</div>
                 </div>
             </div>
         </div>
