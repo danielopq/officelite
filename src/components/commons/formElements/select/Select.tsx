851f5bb7ed0815c34b67b5492851f880f09c29f2
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-11 02:01:29 +0100

    Select.tsx - show and hide submenu ready

--- src/components/commons/formElements/select/Select.tsx
+++ src/components/commons/formElements/select/Select.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import '../formContainer.css';
 import './select.css';
 
@@ -8,14 +9,62 @@ type Props = {
 }
 
 const Select: React.FC<Props> = ({ id, children }) => {
+    const select = useRef<HTMLDivElement>(null);
+    const selection = useRef<HTMLDivElement>(null);
+    const subMenu = useRef<HTMLDivElement>(null);
+
+    const [submenuVisibility, setSubmenuVisibility] = useState(false);
+
+    useEffect(() => {
+        if (subMenu.current) {
+            subMenu.current.style.display = submenuVisibility ? 'flex' : 'none';
+        }
+    }, [submenuVisibility]);
+
+    useEffect(() => {
+        const currentSelect = select.current;
+        const showSubmenu = () => {
+            setSubmenuVisibility(true);
+            changeArrow();
+        };
+
+        const hideSubmenu = () => {
+            setSubmenuVisibility(false);
+            changeArrow();
+        };
+
+        const changeArrow = () => {
+            if (selection.current) {
+                selection.current.className == 'arrowDown' ? selection.current.className = 'arrowUp' : selection.current.className = 'arrowDown';
+            }
+        }
+
+        if (currentSelect) {
+            currentSelect.addEventListener('mouseenter', showSubmenu);
+            currentSelect.addEventListener('mouseleave', hideSubmenu);
+        }
+
+        return () => {
+            if (currentSelect) {
+                currentSelect.removeEventListener('mouseenter', showSubmenu);
+                currentSelect.removeEventListener('mouseleave', hideSubmenu);
+            }
+        };
+    }, []);
+
     return (
         <div className='formContainer'>
-            <div id={id} className='select'>
-                <div id="selectHeader">
-                    <p id="selectedValue"  className='arrowDown'><span className="pack">Basic Pack </span><span className="price">Free</span></p>
+            <div ref={select} className='select'>
+                <div className="selectHeader">
+                    <p id={id} ref={selection} className='arrowDown'>
+                        <span className="pack">Basic Pack </span>
+                        <span className="price">Free</span>
+                    </p>
                 </div>
-                <div id="selectSubmenu">
-                    {children}
+                <div ref={subMenu} className="submenu">
+                    <div className="submenuFrame">
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
