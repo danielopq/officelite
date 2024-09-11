d4f22b26b7899b10733ab14a2b18cd1b10b061d2
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-11 10:37:30 +0100

    Textfield.tsx - error version added

--- src/components/commons/formElements/textField/Textfield.tsx
+++ src/components/commons/formElements/textField/Textfield.tsx
@@ -1,14 +1,17 @@
 import '../formContainer.css';
+import './textField.css'
 
 type Props = {
     id: string;
     children: string;
+    error:boolean;
 }
 
-const TextField: React.FC<Props> = ({ id,children }) => {
+const TextField: React.FC<Props> = ({ id,children,error=false }) => {
+    
     return (
         <div className='formContainer'>
-            <input id={id} name={id} type="text" placeholder={children}></input>
+            <input id={id} name={id} type="text" placeholder={children} className="signUpTextfield"></input>
         </div>
     )
 }
