9db517c5c93b192029fd80e44bc429aa5f83a5b7
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-17 13:38:43 +0100

    plan.css - tablet version

--- src/components/home/homeFooter/Plans/Plans.tsx
+++ src/components/home/homeFooter/Plans/Plans.tsx
@@ -16,18 +16,18 @@ const Plans = () => {
     /**
      * Renders the sign-up page.
      */
-    
-    const navigateToSignUp = (event: React.MouseEvent<HTMLButtonElement>) =>{
-        let selectedPlan:selectedPlanInfo = {selectedPack: '', selectedPackPrice: ''}; 
-        switch(event.currentTarget.id){
+
+    const navigateToSignUp = (event: React.MouseEvent<HTMLButtonElement>) => {
+        let selectedPlan: selectedPlanInfo = { selectedPack: '', selectedPackPrice: '' };
+        switch (event.currentTarget.id) {
             case 'tryBasicButton':
-                selectedPlan = {selectedPack: 'Basic Pack', selectedPackPrice: 'Free'};
+                selectedPlan = { selectedPack: 'Basic Pack', selectedPackPrice: 'Free' };
                 break;
             case 'tryProButton':
-                selectedPlan = {selectedPack: 'Pro Pack', selectedPackPrice: '$9.99'};
+                selectedPlan = { selectedPack: 'Pro Pack', selectedPackPrice: '$9.99' };
                 break;
             case 'tryUltimateButton':
-                selectedPlan = {selectedPack: 'Ultimate Pack', selectedPackPrice: '19.99$'};
+                selectedPlan = { selectedPack: 'Ultimate Pack', selectedPackPrice: '19.99$' };
                 break;
         }
         navigate('/signup', { state: selectedPlan });
@@ -36,30 +36,48 @@ const Plans = () => {
     return (
         <div id="plans">
             <div id="basicPlan" className='planFrame'>
-                <p className='planName'>Basic</p>
-                <p className='planPrice'>Free</p>
-                <p className='userLimit'>Up to 5 users for free</p>
-                <p className='planFeature'>Basic document collaboration</p>
-                <p className='planFeature'>2 GB storage</p>
-                <p className='planFeature'>Great security and support</p>
+                <div className='planDescription'>
+                    <div className='planMainDescription'>
+                        <p className='planName'>Basic</p>
+                        <p className='planPrice'>Free</p>
+                        <p className='userLimit'>Up to 5 users for free</p>
+                    </div>
+                    <div className='planFeatures'>
+                        <p className='feature'>Basic document collaboration</p>
+                        <p className='feature'>2 GB storage</p>
+                        <p className='feature'>Great security and support</p>
+                    </div>
+                </div>
                 <DefaultButton id="tryBasicButton" buttonType='tryDark' onButtonClick={navigateToSignUp}>Try for Free</DefaultButton>
             </div>
             <div id="proPlan" className='planFrame'>
-                <p className='planName'>Pro</p>
-                <p className='planPrice'>$9.99</p>
-                <p className='userLimit'>Per user, billed monthly</p>
-                <p className='planFeature'>All essential integrations</p>
-                <p className='planFeature'>50 GB storage</p>
-                <p className='planFeature'>More control and insights</p>
+                <div className='planDescription'>
+                    <div className='planMainDescription'>
+                        <p className='planName'>Pro</p>
+                        <p className='planPrice'>$9.99</p>
+                        <p className='userLimit'>Per user, billed monthly</p>
+                    </div>
+                    <div className='planFeatures'>
+                        <p className='feature'>All essential integrations</p>
+                        <p className='feature'>50 GB storage</p>
+                        <p className='feature'>More control and insights</p>
+                    </div>
+                </div>
                 <DefaultButton id="tryProButton" buttonType='tryLight' onButtonClick={navigateToSignUp}>Try for Free</DefaultButton>
             </div>
             <div id="ultimatePlan" className='planFrame'>
-                <p className='planName'>Ultimate</p>
-                <p className='planPrice'>$19.99</p>
-                <p className='userLimit'>Per user, billed monthly</p>
-                <p className='planFeature'>Robust work management</p>
-                <p className='planFeature'>100 GB storage</p>
-                <p className='planFeature'>VIP support</p>
+                <div className='planDescription'>
+                    <div className='planMainDescription'>
+                        <p className='planName'>Ultimate</p>
+                        <p className='planPrice'>$19.99</p>
+                        <p className='userLimit'>Per user, billed monthly</p>
+                    </div>
+                    <div className='planFeatures'>
+                        <p className='feature'>Robust work management</p>
+                        <p className='feature'>100 GB storage</p>
+                        <p className='feature'>VIP support</p>
+                    </div>
+                </div>
                 <DefaultButton id="tryUltimateButton" buttonType='tryDark' onButtonClick={navigateToSignUp}>Try for Free</DefaultButton>
             </div>
         </div>
