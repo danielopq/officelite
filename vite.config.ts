a061f87318d6782cdff6cd5eec4c2f76c86f1f61
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-18 13:35:46 +0100

    vite.confug.ts - server modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Permite el acceso desde la red local
+    port: 3000,       // Puedes cambiar este puerto si lo necesitas
+    strictPort: true, // Si el puerto 3000 está ocupado, no intentará otro
+  },
+});
