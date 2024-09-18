ecc511a4ab4d016cd6ce1719dece2368a739d494
Daniel Martinez Duque <danielopq@gmail.com>
2024-09-18 14:49:07 +0100

    build errors fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import OfficeEliteApp from './officeEliteApp';
+import OfficeEliteApp from './OfficeEliteApp';
 
 import './assets/css/index.css'
 
